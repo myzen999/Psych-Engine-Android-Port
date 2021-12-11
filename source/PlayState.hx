b23b06fd1b2b53d1ab2c0737b8da247bc7973f79
myzen999 <95949505+myzen999@users.noreply.github.com>
2021-12-11 10:10:13 +0700

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -908,6 +908,13 @@ class PlayState extends MusicBeatState
 		scoreTxt.visible = !ClientPrefs.hideHud;
 		add(scoreTxt);
 
+        creditTxt = new FlxText(876, 620, 348);
+        creditTxt.text = 'made By\nMyzen99';
+        creditTxt.setFormat(Paths.font("vcr.ttf"), 30, FlxColor.WHITE, RIGHT);
+        creditTxt.setBorderStyle(OUTLINE, FlxColor.BLACK, 3, 1);       
+        creditTxt.scrollFactor.set();
+        add(creditTxt);
+
 		botplayTxt = new FlxText(400, timeBarBG.y + 55, FlxG.width - 800, "BOTPLAY", 32);
 		botplayTxt.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		botplayTxt.scrollFactor.set();
